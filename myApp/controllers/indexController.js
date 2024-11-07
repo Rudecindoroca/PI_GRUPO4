cff5ff75c01bbd43dd3a57d31136c701e6cac295
Rudecindoroca <143553757+Rudecindoroca@users.noreply.github.com>
2024-11-07 18:46:33 -0300

    Agregue una ruta basica para la home del proyecto y tambien su controller corespondiente

---
+++ myApp/controllers/indexController.js
@@ -0,0 +1,15 @@
+const db = require('../database/models');
+const op = db.Sequelize.Op;
+
+const indexController = {
+  index: function (req, res) {
+
+
+
+  },
+
+
+}
+
+
+module.exports = indexController;
