94d531fe637fa59e66c34258f4404eb98b8c107b
Rudecindoroca <143553757+Rudecindoroca@users.noreply.github.com>
2024-11-07 18:25:55 -0300

    cree el proyecto express y tambien ejecute el install del sequelize y el mysql2

---
+++ myApp/database/config/config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "database_development", //agregar el nombre de la base de datos nuestra 
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+ 
+}
