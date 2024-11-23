0fb580cee67b7396d26489bb61410e420b99fc63
Rudecindoroca <143553757+Rudecindoroca@users.noreply.github.com>
2024-11-23 17:05:07 -0300

    Agregue productos a la base de datos

--- myApp/baseDeDatos.sql
+++ myApp/baseDeDatos.sql
@@ -35,3 +35,97 @@ INSERT INTO Products (imagen, nombre_producto, descripcion, user_id) VALUES
 ('producto3.jpg', 'Producto 3', 'Descripción del Producto 3', 1),
 ('producto4.jpg', 'Producto 4', 'Descripción del Producto 4', 3),
 ('producto5.jpg', 'Producto 5', 'Descripción del Producto 5', 4);
+
+
+
+
+
+-- Cambiando los productos para que sean prodcutos de verdad con fotos de verdad:
+
+SELECT * FROM proyecto_db.Products;
+
+UPDATE proyecto_db.Products
+
+SET nombre_producto = 'Gorro Strussy',
+    descripcion = '
+Gorro ajustado tipo skullcap con detalle de borde enrollado y bordado del logo de Stüssy en punto de cadeneta.
+
+Construcción superior de 6 puntos
+Ajuste clásico
+Talla única
+Unisex
+Material: 48% viscosa / 30% poliéster / 22% nailon
+Importado',
+    imagen = 'stussyhat.webp',
+    user_id = 5
+WHERE id = 1;
+
+SELECT * FROM proyecto_db.Products;
+
+UPDATE proyecto_db.Products
+SET nombre_producto = 'Lazy Tee Stussy',
+    descripcion = 'Camiseta de corte relajado en algodón teñido con pigmento, con detalles de costuras al revés. Logo Stüssy smooth stock estampado en el pecho izquierdo.
+
+Manga corta
+Cuello redondo y puños acanalados
+Costuras con puntadas al revés
+Estampado del logo Stüssy smooth stock
+Corte relajado
+Unisex
+Material: 100% algodón
+Importado
+*Esta prenda ha sido teñida individualmente, lo que produce un resultado único. El color puede desvanecerse o desteñirse después del lavado.',
+    imagen = 'LazTee.jpg',
+    user_id = 3
+WHERE id = 2;
+
+SELECT * FROM proyecto_db.Products;
+
+UPDATE proyecto_db.Products
+SET nombre_producto = 'Champion Stussy Tee',
+    descripcion = '
+Camiseta de corte amplio en jersey de algodón de peso medio de 6.5oz. Incluye gráficos estampados con serigrafía.
+
+Manga corta
+Cuello acanalado
+Corte grande
+Unisex
+Material: 100% algodón
+Importado',
+    imagen = 'ChampionTee.jpg',
+    user_id = 2
+WHERE id = 3;
+
+SELECT * FROM proyecto_db.Products;
+
+UPDATE proyecto_db.Products
+SET nombre_producto = 'Camisa Carhart',
+    descripcion = 'Camisa de manga larga a cuadros hecha de resistente sarga de algodón. Ofrece un ajuste relajado para comodidad durante todo el día y cuenta con un cierre de botones al frente y un cuello clásico. Perfecta para usar en capas o por sí sola.
+
+Mangas largas
+Cierre de botones
+Bolsillo en el pecho con detalle de logo discreto
+Corte relajado
+Material: 100% algodón
+Importado
+',
+    imagen = 'ButtonUpCarhart.jpg',
+    user_id = 4
+WHERE id = 4;
+
+
+SELECT * FROM proyecto_db.Products;
+
+UPDATE proyecto_db.Products
+SET nombre_producto = 'Dog crown Tee Stussy',
+    descripcion = 'Camiseta de corte amplio en jersey de algodón de peso medio de 6.5 oz. Incluye gráficos estampados con serigrafía.
+
+Manga corta
+Cuello acanalado
+Corte grande
+Unisex
+Material: 100% algodón
+Importado',
+    imagen = 'DogcrownTEE.jpg',
+    user_id = 1
+WHERE id = 5;
